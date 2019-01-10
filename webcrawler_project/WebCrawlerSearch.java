6c63206cef760a1f77554cb9c065b52215feb02e
Hemavathi <hemavathi.tamilmaran95@gmail.com>
2019-01-10 15:45:36 +0530

    final commit

--- webcrawler_project/WebCrawlerSearch.java
+++ webcrawler_project/WebCrawlerSearch.java
@@ -1,4 +1,3 @@
-package webcrawler_project;
 
 import java.io.IOException;
 import java.util.HashSet;
@@ -7,54 +6,47 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-
-
 public class WebCrawlerSearch {
 	
 	HashSet<String> set =  new HashSet<String>();
-	HashSet<String> images =  new HashSet<String>();
-	HashSet<String> imports =  new HashSet<String>();
+	HashSet<String> result =  new HashSet<String>();
+	
 	public String find() {
-		getURL("http://E:/pcf/LBG/Project/WebContent/index.html");
-		System.out.println(set.size());
-		Iterator it1 = set.iterator();
-		while(it1.hasNext()) {
-				System.out.println((String) it1.next());
-		}
-		Iterator it2 = set.iterator();
-		while(it2.hasNext()) {
-				System.out.println((String) it2.next());
-		}
-		Iterator it3 = set.iterator();
-		while(it3.hasNext()) {
-				System.out.println((String) it3.next());
-		}
+		getURL("http://www.wipro.com");
 		return "check your console";
 	}
 	
 	public void getURL(String a) {
-		
 		Document doc = null;
+		String link = null;
 		try {
-			System.out.println("a value"+a);
+			System.out.println(a);
 			doc = Jsoup.connect(a).get();
-			org.jsoup.select.Elements links = doc.select("a[href]");
-			org.jsoup.select.Elements src = doc.select("[src]");
-			org.jsoup.select.Elements imports = doc.select("link[href]");
+			org.jsoup.select.Elements links = doc.select("a");
+			
 			for(Element e: links) {
-				set.add(e.attr("abs:href"));
+				
+				if(e.attr("abs:href")!=null && e.attr("abs:href").trim()!="") {
+					result.add(a);
+					set.add(e.attr("abs:href"));
+					set.remove(a);
+				}
+				
 			}
-			for(Element e: src) {
-				set.add(e.attr("abs:src"));
-			}
-			for(Element e: imports) {
-				set.add(e.attr("abs:href"));
-			}
+
 			Iterator it = set.iterator();
 			while(it.hasNext()) {
-				String link = (String) it.next();
-				if(link != null && link.trim() != "") {
+				
+				link = (String) it.next();
+				if(link != null && link.trim() != "" && !result.contains(link)) {
 					getURL(link);
+				}
+				else {
+					String skipLink = link;
+					if(!(result).contains(skipLink)) {
+						set.remove(link);
+						getURL(skipLink);
+					}
 				}
 			}
 		}
