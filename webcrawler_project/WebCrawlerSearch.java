4efe0c8aac188cf482bad8288d0467fe37c3a88b
Hemavathi <hemavathi.tamilmaran95@gmail.com>
2019-01-10 17:58:15 +0530

    final updated

--- webcrawler_project/WebCrawlerSearch.java
+++ webcrawler_project/WebCrawlerSearch.java
@@ -1,5 +1,6 @@
 package webcrawler_project;
 
+import java.util.ConcurrentModificationException;
 import java.util.HashSet;
 import java.util.Iterator;
 import org.jsoup.Jsoup;
@@ -12,11 +13,10 @@ public class WebCrawlerSearch {
 	HashSet<String> result =  new HashSet<String>();
 	HashSet<String> webImages =  new HashSet<String>();
 	HashSet<String> webExternal =  new HashSet<String>();
+	String url = "http://www.wipro.com"; 
 	
 	public String find() {
-		String url = "http://www.wipro.com"; 
 		getURL(url);
-			
 		//---------------------------print images ------------------------------
 		System.out.println("Images from ->"+url);
 		Iterator img = webImages.iterator();
@@ -30,14 +30,16 @@ public class WebCrawlerSearch {
 		while(ext.hasNext()) {
 			System.out.println(ext.next());
 		}
-		
+		System.out.println("completed");
+
 		return "check your console";
 	}
 	
-	public void getURL(String a) {
+	public void getURL(String a) throws ConcurrentModificationException {
 		Document doc = null;
 		String link = null;
 		try {
+			
 			doc = Jsoup.connect(a).get();
 			org.jsoup.select.Elements links = doc.select("a[href]");
 			org.jsoup.select.Elements images = doc.select("[src]");
@@ -49,51 +51,55 @@ public class WebCrawlerSearch {
 					result.add(a);
 					set.add(e.attr("abs:href"));
 					set.remove(a);
-				}	
+				}
+				
 			}
 			
 			//------------------------images---------------------------------------------
-			for (Element src : images) {
-	            if (src.attr("abs:src")!=null && src.attr("abs:src").trim()!="") {
-	            	webImages.add(src.attr("abs:src"));
+			for (Element e : images) {
+	            if (e.attr("abs:src")!=null && e.attr("abs:src").trim()!="") {
+	            	webImages.add(e.attr("abs:src"));
 	            }
 	            	
 			}
 			
 			//------------------------external links---------------------------------------------
-			for (Element externalLink : external) {
-				if (externalLink.attr("abs:href")!=null && externalLink.attr("abs:href").trim()!="") {
-					webExternal.add(externalLink.attr("abs:href"));
-	            }				
-			}			
-			set.removeAll(webImages);
-			set.removeAll(webExternal);
+			for (Element e : external) {
+				if (e.attr("abs:href")!=null && e.attr("abs:href").trim()!="") {
+					webExternal.add(e.attr("abs:href"));
+	            }
+				
+			}
 
 			//---------------------iterator for links----------------------------------------------
 			Iterator it = set.iterator();
 			while(it.hasNext()) {
 				
-				link = (String) it.next();
+				link = (String) it.next()+"";
 				if(link != null && link.trim() != "" && !result.contains(link) && link.contains(".html")) {
-					System.out.println("if :"+link);
+					System.out.println(link);
 					getURL(link);
 				}
 				else {
 					String skipLink = link;
-					if(!(result).contains(skipLink) && skipLink.contains(".html")) {
+					if(!(result).contains(skipLink)) {
 						set.remove(link);
-						System.out.println("else :"+skipLink);
+						System.out.println(skipLink);
 						getURL(skipLink);
-					}					
+					}
+					
 				}
 			}
+			
 		}
 		catch (Exception e) {
 			e.printStackTrace();
-		}		
-	}	
+		}
+		
+	}
+	
 	public static void main(String[] args) {		
 		WebCrawlerSearch web = new WebCrawlerSearch();
 		web.find();		
-	}	
+	}
 }
